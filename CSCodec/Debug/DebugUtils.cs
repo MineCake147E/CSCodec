1dd3fcaee0acaea40fe9110b0366728e5d662818
MineCake147E <MineCake1.4.7@outlook.com>
2019-02-14 17:20:32 +0900

    added BitWriter

---
+++ CSCodec/Debug/DebugUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCodec.Debug
+{
+	/// <summary>
+	/// Debugging utilities
+	/// </summary>
+	public static class DebugUtils
+	{
+		/// <summary>
+		/// Dumps the array to the Console.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="array">The array.</param>
+		/// <param name="format">The formatting codes.</param>
+		public static void DumpArray<T>(Span<T> array, string format)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine(format, array[i]);
+			}
+		}
+	}
+}
